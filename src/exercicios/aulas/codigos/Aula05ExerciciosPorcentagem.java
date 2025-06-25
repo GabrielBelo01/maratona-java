6d902a3eb18a7530bc5e2e56aee283d1a55770a0
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2025-06-25 12:32:17 -0300

    exercicios-aulas

---
+++ src/exercicios/aulas/codigos/Aula05ExerciciosPorcentagem.java
@@ -0,0 +1,32 @@
+package exercicios.aulas.codigos;
+
+import java.util.Scanner;
+
+public class Aula05ExerciciosPorcentagem {
+    // 0 a 34.712  = 9.70%
+    // 34.713 a 68.507 = 37.35%
+    // 68.508       = 49.50%
+    public static void main(String[] args) {
+        double salario;
+        double percentual;
+        double valorImposto;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual o seu salario?");
+        salario = sc.nextDouble();
+
+        if (salario <= 34712){
+            percentual = 9.70 / 100;
+            valorImposto = salario * percentual;
+        } else if (salario >= 34713 && salario <= 68507) {
+            percentual = 37.35 / 100;
+            valorImposto = salario * percentual;
+        } else{
+            percentual = 49.50 / 100;
+            valorImposto = salario * percentual;
+        }
+
+        System.out.println("Voce tera que pagar "+valorImposto+" de imposto");
+        System.out.println("E seu salario ficara:" +(salario-valorImposto));
+
+    }
+}
---
+++ src/maratonajava/aulas/codigos/Aula05EstruturasCondicionais05.java
@@ -0,0 +1,40 @@
+package maratonajava.aulas.codigos;
+
+import java.util.Scanner;
+
+public class Aula05EstruturasCondicionais05 {
+    public static void main(String[] args) {
+        // imprimir o dia da semana, considerando 1 como domingo
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual dia da semana estamos?");
+        int dia = sc.nextInt();
+
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("Numero invalido");
+                break;
+        }
+        System.out.println("--verficação do dia concluida--");
+    }
+}
