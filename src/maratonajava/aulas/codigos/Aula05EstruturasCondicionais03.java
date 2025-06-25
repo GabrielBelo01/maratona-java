c3b939394685e078d720d07ae840156c1ceaf73a
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2025-06-24 23:02:46 -0300

    aulas-exercicios

---
+++ src/maratonajava/aulas/codigos/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,23 @@
+package maratonajava.aulas.codigos;
+
+import java.util.Scanner;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        double salario;
+        String mensagemDoar = "Eu vou doar 500 reais";
+        String mensagemNaoDoar = "Eu nao consigo, mas um dia vou conseguir";
+
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Qual seu salario?");
+        salario = sc.nextDouble();
+
+        String resultado = salario >= 5000 ? mensagemDoar : mensagemNaoDoar;
+        System.out.println(resultado);
+
+        // OU
+
+        System.out.println(salario >= 5000 ? mensagemDoar : mensagemNaoDoar);
+    }
+
+}
